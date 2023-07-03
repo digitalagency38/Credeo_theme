29e96d5cd194d2a6248fc346225f50d5142a6cf9
Гончаров Сергей <Goncharov-5769@main-fer.fnpz.uz>
2023-07-03 17:05:05 +0500

    Доработка меню

--- js/main_dop.js
+++ js/main_dop.js
@@ -49,6 +49,34 @@ window.config = {
     }
 };
 $(function() {
+    var positions = [], //сюда сложим на загрузке страницы позиции наших "якорных" блоков, чтобы не считать их каждый раз. и сюда же положим ссылки на соответствующие a.scroll-to
+    currentActive = null, //здесь будет храниться id текущего блока, чтобы не менять классы по 100 раз за одну прокрутку 
+    links = $('#menu-menyu li a'); //сохраним массив всех a.scroll-to
+
+    $("section").each(function(){ //перебираем блоки, сохраняем позиции и ссылки на пункты меню
+        positions.push({
+            top: $(this).position().top - 100,
+            a: links.filter('[href="#'+$(this).attr('id')+'"]')
+        });
+    });
+    
+    //делаем реверс массива, чтобы при скролле перебирать его с конца и выходить из цикла при нахождении
+    //зачем нам проверять каждый блок, если прокрутка уже ниже последнего, верно?
+    positions = positions.reverse(); 
+    
+    $(window).on('scroll',function() {
+        var winTop = $(window).scrollTop();
+        for(var i = 0; i < positions.length; i++){
+            if(positions[i].top < winTop){ //если прокрутка страницы ниже нашего блока
+                if(currentActive !== i){ //и если мы еще не добавили класс текущему блоку
+                    currentActive = i;
+                    links.filter('.isActive').removeClass('isActive'); //снимаем класс .active с текущего пункта меню
+                    positions[i].a.addClass("isActive");
+                }
+                break; //выходим из цикла, не нужно проверять блоки, которые выше
+            }
+        }
+    });
     jQuery(document).ready(function($){
         $('.header__menu ul').flexMenu({
             showOnHover: false,
